ac4875547e37e436de01335dbbdf577bd6674752
AlbeTorres <albertocorreoficial@gmail.com>
2022-02-24 12:43:28 -0500

    crear modelo insertar bd

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -1,3 +1,20 @@
-exports.crearUsuario=(req,res)=>{
-    console.log(req.body);
+const Usuario = require('../models/User');
+
+exports.crearUsuario= async(req,res)=>{
+    try {
+        let usuario;
+
+        //crea nuevo user
+        usuario = new Usuario(req.body);
+
+        //guardar usuario
+        await usuario.save();
+
+        console.log('Usuario creado correctamente');
+        res.send('Usuario creado correctamente')
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('hubo un error');
+        
+    }
 }
