1ca5a7f8e36bad0857066838a33bcbe880b3803e
AlbeTorres <albertocorreoficial@gmail.com>
2022-02-24 10:52:13 -0500

    conectada bd local

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require('cors');
+const conectarDB = require("./config/db");
 
+//crear el servidor
 const app = express();
+
+//conectar a la base de datos
+conectarDB();
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+
+//Corriendo el server
+app.listen(PORT, ()=>{
+
+    console.log(`Server corriendo en el puerto ${PORT}`);
+});
+
