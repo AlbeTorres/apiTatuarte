7f05a07d2647faf763ee13db677f58b133be87ce
AlbeTorres <albertocorreoficial@gmail.com>
2022-03-23 10:45:25 -0400

    ni idea

--- controllers/userscontroller.js
+++ controllers/userscontroller.js
@@ -56,3 +56,124 @@ exports.crearUsuario= async(req,res)=>{
         
     }
 }
+
+
+//Obtiene todos los usuarios
+//obtiene un usuario filtrado /usuario?_id=id
+exports.obtenerUsuarios= async(req,res)=>{
+    //extraer los filtros del req
+    const filters = req.query;
+
+    try {
+        //comprobar si los filtros están vacíos
+        if (filters !={}){
+            
+            //comprobar que existan usuarios con esos filtros
+            const user= await Usuario.find(filters);
+
+            if(!user){
+                return res.status(404).json({msg:'No existe ese usuario'});
+            }
+
+            //retornar usuarios
+            return res.json({user});
+        }
+
+        //comprobar que existe el usuario
+        const user = await Usuario.find();
+
+        if(!user){
+            return res.status(404).json({msg:'No existen usuarios'});
+        }
+
+        //retornar los usuarios
+        res.json({user});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg:'hubo un error'});
+        
+    }
+
+}
+
+exports.modificarUsuario= async(req,res)=>{
+
+     //Revisar si hay errores
+     const errores = validationResult(req);
+
+     if(!errores.isEmpty()){
+         return res.status(400).json({errores: errores.array()});
+     }
+
+     try {
+
+        //comprobar que existe el usuario
+        const user = await Usuario.findById(req.params.id);
+
+        if(!user){
+            return res.status(404).json({msg:'No existe ese usuario'});
+        }
+
+        //comprobar que el usuario pertenezca al user logueado
+        if(user._id != req.usuario.id){
+            return res.status(401).json({msg:'No tiene permisos para actualizar este usuario'});
+        }
+
+        //extraer password
+        const {password}= req.body;
+
+        //crea nuevo user
+       const usuario = req.body;
+
+
+        //hashear el password
+        const salt = await bcryptjs.genSalt(10);
+
+        usuario.password = await bcryptjs.hash(password, salt);
+
+        console.log(usuario)
+        //actualizar usuario
+        await Usuario.updateOne({_id:req.params.id},{$set:usuario});
+
+        res.json({msg:"Usuario actualizado correctamente"})
+
+         
+     } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:'hubo un error'});
+     }
+
+    
+
+}
+
+exports.eliminarUsuario= async(req,res)=>{
+
+    try {
+
+        //comprobar que existe el usuario
+        const user = await Usuario.findById(req.params.id);
+
+        if(!user){
+            return res.status(404).json({msg:'No existe ese usuario'});
+        }
+
+        //comprobar que el usuario pertenezca al user logueado
+        if(user._id != req.usuario.id){
+            return res.status(401).json({msg:'No tiene permisos para eliminar este usuario'});
+        }
+
+        //actualizar usuario
+        await Usuario.findOneAndRemove({_id:req.params.id});
+
+        res.json({msg:"Usuario eliminado correctamente"})
+
+         
+     } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:'hubo un error'});
+     }
+
+
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router= express.Router();
 const usuarioController= require('../controllers/userscontroller');
 const {check} = require('express-validator');
+const auth = require('../middleware/auth');
 
 //Crea un usuario
 //api/usuarios
@@ -13,5 +14,21 @@ router.post('/',[
 ],
     usuarioController.crearUsuario
 );
+
+//Obtiene todos los usuarios
+//obtiene un usuario filtrado /usuario?_id=id
+router.get('/',auth,usuarioController.obtenerUsuarios);
+
+//modifica un usuario por id
+router.patch('/:id',auth,[
+    check('nombre','El nombre no puede estar vacio').not().isEmpty(),
+    check('password',' La contraseña debe tener como mínimo 8 caracteres').isLength({min:8}),
+    check('provincia', 'La provincia no debe estar vacía').not().isEmpty(),
+    check('municipio', 'El municipio no debe estar vacío').not().isEmpty()
+], usuarioController.modificarUsuario);
+
+//elimina un usuario por id
+router.delete('/:id',auth,usuarioController.eliminarUsuario);
+
 
 module.exports= router;
